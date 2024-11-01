5f02f895ae1923f0513f050aedc36d3fa132f9fb
Indicium199 <jennygreer805@gmail.com>
2024-11-01 15:35:53 +0000

    Added a Welcome tab

--- dashapp.py
+++ dashapp.py
@@ -8,11 +8,12 @@ app = dash.Dash(__name__)
 # Define the app layout with tabs
 app.layout = html.Div([
     dcc.Tabs(id='tabs', value='tab1', children=[
-        dcc.Tab(label='Barclays Plc', value='tab1'),
-        dcc.Tab(label='HSBC', value='tab2'),
-        dcc.Tab(label='Natwest Group', value='tab3'),
-        dcc.Tab(label='Lloyds', value='tab4'),
-        dcc.Tab(label='Standard Chartered', value='tab5'),
+        dcc.Tab(label='Welcome', value='tab1'),
+        dcc.Tab(label='Barclays Plc', value='tab2'),
+        dcc.Tab(label='HSBC', value='tab3'),
+        dcc.Tab(label='Natwest Group', value='tab4'),
+        dcc.Tab(label='Lloyds', value='tab5'),
+        dcc.Tab(label='Standard Chartered', value='tab6'),
     ]),
     html.Div(id='tabs-content')
 ])
@@ -25,27 +26,32 @@ app.layout = html.Div([
 def render_content(tab):
     if tab == 'tab1':
         return html.Div([
-            html.H3('Welcome to Bank A'),
-            html.P('Here you can manage your accounts and view your transactions.')
+            html.H2('FTSE 100 Listed Banks Dashboard'),
+            html.P('The purpose of this dashboard is to...')
         ])
     elif tab == 'tab2':
         return html.Div([
-            html.H3('Welcome to Bank B'),
+            html.H3('Barclays'),
             html.P('Here you can view loan options and apply for a loan.')
         ])
     elif tab == 'tab3':
         return html.Div([
-            html.H3('Welcome to Bank C'),
+            html.H3('HSBC'),
             html.P('Explore investment options and check your portfolio.')
         ])
     elif tab == 'tab4':
         return html.Div([
-            html.H3('Welcome to Bank D'),
+            html.H3('Natwest Group'),
             html.P('Find the best savings accounts and rates here.')
         ])
     elif tab == 'tab5':
         return html.Div([
-            html.H3('Welcome to Bank E'),
+            html.H3('Lloyds'),
+            html.P('Access credit card offers and rewards information.')
+        ])
+    elif tab =='tab6':
+        return html.Div([
+            html.H3('Standard Chartered'),
             html.P('Access credit card offers and rewards information.')
         ])
 
